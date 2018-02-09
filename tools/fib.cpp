e9fc21386a1d3ba70859d25f46a5a713c8a42e58
Krzysztof Bieleń <kzeslaf@gmail.com>
2018-02-09 17:27:05 +0100

    Test C++14 digit separators.

--- tools/fib.cpp
+++ tools/fib.cpp
@@ -10,6 +10,8 @@ const char *USAGE =
     "Usage:\n"
     "\t./fib NUMBER\n";
 
+const int DIGIT_SEPARATORS_TEST = 1'000'000;
+
 }  // namespace
 
 int main(int argc, char *argv[]) {
@@ -19,5 +21,6 @@ int main(int argc, char *argv[]) {
   }
 
   printf("%d\n", fib(atoi(argv[1])));
+  printf("%d\n", DIGIT_SEPARATORS_TEST);
   return 0;
 }
